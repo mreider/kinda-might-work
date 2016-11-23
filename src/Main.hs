33c04d52ac64b2deee5637832a17f46d5de25424
Alejandro D. P <vwwv@correo.ugr.es>
2016-11-23 13:08:39 +0100

    Certificates
    - Use htts.
    - Add self signed certificates.
    - Add google credentials.

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,14 @@ import           DB
 import           PageTemplate
 
 import           Protolude
+import           Data.Aeson
 import           Servant.API
 import           Servant.Client
 import           Servant.HTML.Lucid
 import           Servant.Server
 import           Network.Wai                  (Application)
-import           Network.Wai.Handler.Warp     (run)
-
+import           Network.Wai.Handler.Warp     (run,setPort,defaultSettings)
+import           Network.Wai.Handler.WarpTLS
 
 
 
@@ -35,32 +36,74 @@ type SyncTrelloWunder   = "sync"   :> Get [HTML,JSON] Page
 type IndexPage          =             Get [HTML,JSON] Page   
 
 
+{-
+<html>
+  <head>
+    <meta name="google-site-verification" content="auj20bE8W7nWGXthtzXrkuv4f4u6-VXZ9fQBNLefIlo" />
+    <title> My title </title>
+  </head> 
+  <body>
+    page contents
+  </body>
+</html>
+
+-}
 
 
 api :: Proxy KindaMightWork_API
 api = Proxy
 
 
-server :: Server KindaMightWork_API
-server =    return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
-       :<|> return LandingPage
+server :: PubCred -> PrivCred -> Server KindaMightWork_API
+server PubCred{..} PrivCred{..} =    landPage
+                                :<|> landPage
+                                :<|> landPage
+                                :<|> landPage
+                                :<|> landPage
+                                :<|> landPage
+                                :<|> landPage
+                                :<|> landPage
+  where
+    landPage = return (LandingPage, site_verification)
+
+
+data PubCred = PubCred
+      { google_client_id  :: Text
+      , site_verification :: Text
+      } deriving(Show,Read,Eq,Ord,Generic,ToJSON,FromJSON)
+
+data PrivCred   = PrivCred
+      { google_secret      :: Text
+      } deriving(Show,Read,Eq,Ord,Generic,ToJSON,FromJSON)
 
 
 main :: IO ()
-main = do putStrLn $ "Running on port "<> (show port::Text)   
-          run port . serve api $ server 
+main = do pub_conf  <- decode.toSL <$> readFile pub_file
+          priv_conf <- decode.toSL <$> readFile priv_file
+          
+          case (pub_conf,priv_conf) of
+
+            (Nothing , _        ) -> putStrLn $ "Could not parse: "<> pub_file
+            
+            (_       , Nothing  ) -> putStrLn $ "Could not parse: "<> priv_file
+
+            (Just pub, Just priv) -> runTLS tls setttings
+                                          . debuging 
+                                          . serve api 
+                                          $ server pub priv
+
  where
-    port = 443
+
+    pub_file  = "conf/public.json"
+    priv_file = "conf/private.json"
+
+    tls       = tlsSettings "conf/app.crt" "conf/app.key"
+    setttings = defaultSettings & setPort port
+    port      = 443
 
 
-
-
+debuging :: Application -> Application
+debuging app req cont = print req >> app req cont
 
 
 
--- src/PageTemplate.hs
+++ src/PageTemplate.hs
@@ -9,15 +9,25 @@ import           Lucid
 import           Protolude
 import           Data.Aeson
 
-data Page     = LoggedPage
+type Page     = (PageKind,Text)
+data PageKind = LoggedPage
               | LandingPage
               | SyncResult
               deriving(Show,Read,Eq,Ord,Generic,ToJSON)
 
 
 instance ToHtml Page where
+    toHtml  x    = do doctype_    
+                      html_       $ do 
+                        
+                        header_   $ do 
+                        
+                          meta_ [name_    "google-site-verification", content_ (snd x)]
+                          meta_ [charset_ "UTF-8"]
+                          meta_ [title_   "Kinda might work" ]
+                        
+                        body_ (toHtmlRaw x)
+
+
     toHtmlRaw _ = h1_ "This page is under construction"
-    toHtml      = toHtmlRaw
-
-
 
