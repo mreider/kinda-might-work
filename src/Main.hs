bb1c193e1040d8204977c43cda23a201267bbc80
Alejandro D. P <vwwv@correo.ugr.es>
2016-12-12 05:22:52 +0100

    Update conf every call
    - Update configuration on each request.
    - The only exception is DB configuration, that one is kept.

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,123 @@
-module Main where
-  
-import           Server
-import           DB
-import           API
 
-import           Data.Aeson
+module Main where
+
+import           Conf 
+import           API
+import           Profile
+import           OAuth
+import           Synchro
+import           TokenCSFR
+import           WebPage
+
 import           Conf                         (_postgresConn)          
+import           Control.Lens
+import           Data.Aeson
+import           Data.UUID
+import           DB
 import           Network.Wai                  (Application)
 import           Network.Wai.Handler.Warp     (setPort,defaultSettings)
 import           Network.Wai.Handler.WarpTLS
-import           Protolude                  hiding(to,get)
-import           Servant.Server
+import           Protolude                  hiding(to,get,(&))
 import           Servant.API
-import           Servant.Utils.StaticFiles    (serveDirectory)
---------------------------------------------------------------------------
+import           Servant.Server
+import           Servant.Utils.StaticFiles (serveDirectory)
+import           Servant.WithSession
 
 main :: IO ()
-main = do raw_conf  <- decode.toSL <$> readFile conf_file
+main = do raw_conf  <- runExceptT configuration
           case raw_conf of
 
-            Nothing   -> putStrLn $ "Could not parse: "<> conf_file
+            Left ServantErr{..} -> putStrLn errBody
             
-            Just conf -> do db <- getDB $ _postgresConn conf
-                            runTLS tls setttings
-                                 . serve api 
-                                 $ server db conf :<|> serveDirectory "./static"
+            Right conf          -> do db <- getDB $ _postgresConn conf
+                                      runTLS tls setttings
+                                          . serve api 
+                                          $ server db :<|> serveDirectory "./static"
  where
-
-    conf_file  = "conf/public.json"
 
     tls       = tlsSettings "conf/app.crt" "conf/app.key"
     setttings = defaultSettings & setPort port
     port      = 443
+
+
+
+
+---------------------------------------------------------------------------
+server :: DB -> Server KindaMightWork_API
+server db session =    callback  withGoogle _googleCred
+                  :<|> callback' withTrello
+                  :<|> callback  withWuList _wuListCred 
+                  :<|> (loggingOut db session)
+                  :<|> remove Trello
+                  :<|> remove WuList
+                  :<|> syncTrelloWunder
+                  
+                  :<|> indexPage
+
+  where
+
+    -- TODO: update and get profile different!
+
+    callback' :: (TrelloProfile -> PartialProfile)
+              -> Maybe Text
+              -> ExceptT ServantErr IO ()
+    
+    callback' setter = \case
+                         Just c  -> do updateProfile db session $ setter (TrelloProfile c)
+                                       return ()
+                           
+                         Nothing -> return ()
+
+
+
+    callback :: (FromJSON r,Show r) => ( r -> PartialProfile)
+                             -> (Creds  -> OAuthCred)
+                             -> Maybe Text
+                             -> ExceptT ServantErr IO ()
+    
+    callback setter getter = \case
+                               Just c  -> do creds   <- configuration
+                                             token   <- postCode (getter creds) c
+                                             updateProfile db session $ setter token
+
+                               
+                               Nothing -> return ()
+
+
+    generatePage creds = return . webPage (csfrChallenge (_csfrSecret creds) session) creds
+    
+
+    remove  service csfr = do creds   <- configuration
+                              checkTokenCSFR    session creds csfr
+                              removeService  db session service
+                               
+    
+    -- TODO: it should parametrize which board to sync..
+    syncTrelloWunder  csfr (OurForm boards) = do creds   <- configuration
+                                                 checkTokenCSFR session creds csfr
+                                                 syncProfile db session creds boards
+
+
+
+    indexPage        = do creds   <- configuration
+                          profile <- getProfile db session creds
+                          generatePage creds profile
+
+
+-- TODO: implement
+checkTokenCSFR :: UUID -> Creds -> TokenCSFR -> ExceptT ServantErr IO () 
+checkTokenCSFR _ _ _ = return ()
+
+
+
+configuration :: ExceptT ServantErr IO Creds
+configuration =  do raw <- decode.toSL <$> liftIO (readFile conf_file)
+                    maybe problems return raw 
+ where
+  conf_file  = "conf/public.json"
+
+  problems = throwError err500{errBody = "Could not parse: "<> toSL conf_file}
+
+
+
 
